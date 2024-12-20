96d72d61f2aa942bab068d3f42d7ceeace38a2ce
AravinthR2024 <aravinth24122000@gmail.com>
2024-12-20 23:48:55 +0530

    Update sketch.js

--- HBD-Thozhi-2024/Solar_System/sketch.js
+++ HBD-Thozhi-2024/Solar_System/sketch.js
@@ -1,24 +1,3 @@
-// Solar System in Processing - Part 3 (3D textures)
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/009-solarsystemgenerator3d-texture.html
-// https://youtu.be/FGAwi7wpU8c
-// https://editor.p5js.org/codingtrain/sketches/SD8a6k6A
-
-// Since PeasyCam is only for Java, this instead uses p5.EasyCam,
-// which is based on PeasyCam but is made for p5.js.
-// It can be found here: https://github.com/freshfork/p5.EasyCam
-
-// A significant difference from the Processing version is that p5.js
-// does not have the PShape object, so we cannot use that to store the
-// geometry along with its texture and so on. Instead, we have to apply
-// the texture image every time through draw(), right before drawing
-// the sphere that the texture should be applied to.
-//
-// Luckily, this is not difficult - we simply have to make the Planet
-// store the chosen texture image instead of the shape that uses it,
-// and instead of replacing the call to sphere() with a call to shape()
-// we add a call to texture() right before the sphere() call.
-
 let sun;
 let cam;
 
